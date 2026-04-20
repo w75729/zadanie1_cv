0cf4db7bb63982593610916220bb024c57da0c03
w75729 <furkanuniversity66@gmail.com>
2026-04-20 11:19:40 +0200

    75729 - utworzenia script.js

---
+++ zad5/script.js
@@ -0,0 +1,16 @@
+// Numer indeksu: 75729 | Furkan Akgun
+
+function zmienMotyw(kolor) {
+    const link = document.getElementById('theme-link');
+    if (link) { link.href = kolor + '.css'; }
+}
+function toggleSekcja() {
+    const sekcja = document.getElementById('sekcja-projekty');
+    if (sekcja) {
+        if (sekcja.style.display === "none") {
+            sekcja.style.display = "block";
+        } else {
+            sekcja.style.display = "none";
+        }
+    }
+}
